17ac51156be133c67a503b45c5863c6144f28adf
Colin Gorrie <colin@colingorrie.com>
2015-05-06 12:10:40 -0400

    Connect to Flaredown account, populate unique daily visits by browser area chart

--- assets/js/fd_dashboard.js
+++ assets/js/fd_dashboard.js
@@ -1,19 +1,18 @@
 var client = new Keen({
-projectId: "5368fa5436bf5a5623000000",
-readKey: "3f324dcb5636316d6865ab0ebbbbc725224c7f8f3e8899c7733439965d6d4a2c7f13bf7765458790bd50ec76b4361687f51cf626314585dc246bb51aeb455c0a1dd6ce77a993d9c953c5fc554d1d3530ca5d17bdc6d1333ef3d8146a990c79435bb2c7d936f259a22647a75407921056"
+  projectId: "55327e0646f9a75ef4402fc1",
+  readKey: "6c4dfeb182ebda9bb824420a5c6982c8f70309436bf92772e92f8134f016b0dbbcf554e221415c31e9a182f568b3339ebdad420271e3c36e0ebaa06b6756328bf01c0b1f27fcee5887b3e4bd295f0529035d4429dccd7da6632e3c4922bf045b249c77473236e83dc6ee3daac7e2e625"
 });
+
 Keen.ready(function(){
 // ----------------------------------------
 // Sample one
 // ----------------------------------------
-var pageviews_timeline = new Keen.Query("count", {
+var pageviews_timeline = new Keen.Query("count_unique", {
   eventCollection: "pageviews",
-  interval: "hourly",
-  groupBy: "user.device_info.browser.family",
-  timeframe: {
-    start: "2014-05-04T00:00:00.000Z",
-    end: "2014-05-05T00:00:00.000Z"
-  }
+  targetProperty: "permanent_tracker",
+  interval: "daily",
+  groupBy: "user_agent.browser.name",
+  timeframe: "previous_2_weeks"
 });
 client.draw(pageviews_timeline, document.getElementById("grid-1-1"), {
   chartType: "areachart",
